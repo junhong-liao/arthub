1b4e80a7df86e548b28b0142ff96a5e51413f142
junhongliao77@gmail.com <junhongliao77@gmail.com>
2024-08-15 11:28:51 -0400

    configured header

--- arthub/app/components/Header.tsx
+++ arthub/app/components/Header.tsx
@@ -12,6 +12,7 @@ import { FaUser } from "react-icons/fa";
 function Header() {
   const { data: session } = useSession()
   console.log(session)
+  
   return (
     <div className='flex justify-between gap-3 md:gap-2 items-center p-6'>
         <Image 
@@ -24,7 +25,7 @@ function Header() {
         {/* <button className='bg-black text-white p-2 px-4 rounded-full'>Clubs</button> */}
         <button className='font-semibold p-2'>Clubs</button>
 
-        <div className='bg-[#e9e9e9] p-3 flex gap-3 items-center rounded-full'>
+        <div className='bg-[#e9e9e9] p-3 flex gap-2 items-center rounded-full'>
             <HiSearch className='text-[25px] text-gray-500'/>
             <input 
                 type='text' placeholder='Search'
