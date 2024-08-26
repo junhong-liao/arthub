7e81a0d066a7fb70058c1c14ac90d0416c604870
junhongliao77@gmail.com <junhongliao77@gmail.com>
2024-08-25 21:55:40 -0400

    Configured user profile page. Refactored email handling

--- arthub/app/[userId]/page.tsx
+++ arthub/app/[userId]/page.tsx
@@ -3,35 +3,41 @@
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import { useEffect } from "react";
 import app from "../Shared/firebaseConfig";
+import UserInfo from "../../components/UserInfo";
 
 function Profile({ params }: any) {
   const db = getFirestore(app);
 
-  // logs userId
+  // Retrieve user data from Firestore
+  const getUserInfo = async (email: string) => {
+    const docRef = doc(db, "user", email);
+    try {
+      const docSnap = await getDoc(docRef);
+      // CONSOLE PRINT DATA, FOR TESTING PURPOSES ONLY (TO BE DELETED).
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.warn("No documents found.");
+      }
+    } catch (error) {
+      console.error("Error fetching document:", error);
+    }
+  };
+
+  // Logs userId and fetches user info
   useEffect(() => {
-    const userId = params.userId.replace("%40", "@");
-    console.log("Loaded user profile for: " + userId);
-    if (params) {
+    if (params && params.userId) {
+      const userId = params.userId.replace("%40", "@");
+      console.log("Loaded user profile for: " + userId);
       getUserInfo(userId + "@gmail.com");
     }
   }, [params]);
 
-  // retrieve user data from firestore
-
-  const getUserInfo = async ({ email }: any) => {
-    const docRef = doc(db, "user", email);
-    const docSnap = await getDoc(docRef);
-
-    // CONSOLE PRINT DATA, FOR TESTING PURPOSES ONLY ( TO BE DELETED ).
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // docSnap.data() will be undefined in this case
-      console.warn("No documents found.");
-    }
-  };
-
-  return <div>Profile Page</div>;
+  return (
+    <div>
+      <UserInfo />
+    </div>
+  );
 }
 
 export default Profile;
